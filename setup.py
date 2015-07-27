cbb1a82de4e6e21e27ef9e69574961ffed9309e8
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-07-26 19:04:27 -0700

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='python-socketio',
-    version='0.2.0',
+    version='0.3.0',
     url='http://github.com/miguelgrinberg/python-socketio/',
     license='MIT',
     author='Miguel Grinberg',
