7b2e4ab88789a52f1e91be538a1b5da6fb8dff02
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-08-19 23:53:32 -0700

    Updated example app to use gevent websocket if available

--- example/app.py
+++ example/app.py
@@ -105,6 +105,15 @@ if __name__ == '__main__':
     elif async_mode == 'gevent':
         # deploy with gevent
         from gevent import pywsgi
-        pywsgi.WSGIServer(('', 5000), app).serve_forever()
+        try:
+            from geventwebsocket.handler import WebSocketHandler
+            websocket = True
+        except ImportError:
+            websocket = False
+        if websocket:
+            pywsgi.WSGIServer(('', 5000), app,
+                              handler_class=WebSocketHandler).serve_forever()
+        else:
+            pywsgi.WSGIServer(('', 5000), app).serve_forever()
     else:
         print('Unknown async_mode: ' + async_mode)
