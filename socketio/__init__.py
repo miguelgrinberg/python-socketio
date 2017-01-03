c4141bbb87c39ab4ee1bcbf2945b3899b8e75a3c
Miguel Grinberg <miguelgrinberg50@gmail.com>
2017-01-02 22:48:14 -0800

    Release 1.6.2

--- socketio/__init__.py
+++ socketio/__init__.py
@@ -6,7 +6,7 @@ from .redis_manager import RedisManager
 from .server import Server
 from .namespace import Namespace
 
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 
 __all__ = [__version__, Middleware, Server, BaseManager, PubSubManager,
            KombuManager, RedisManager, Namespace]
