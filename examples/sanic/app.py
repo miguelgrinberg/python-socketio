180b8ab7cf2f4c46f4a1d06ac7d5167de8d75a73
xuyaoqiang <xuyaoqiang@gmail.com>
2017-08-01 16:03:49 +0800

    reproduce invalid namespace

--- examples/sanic/app.py
+++ examples/sanic/app.py
@@ -1,93 +1,43 @@
-import asyncio
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: xuyaoqiang
+@contact: xuyaoqiang@gmail.com
+@date: 2017-07-28 15:29
+@version: 0.0.0
+@license:
+@copyright:
+
+"""
 
 from sanic import Sanic
-from sanic.response import html
-
+from sanic.response import html 
 import socketio
+
 
 sio = socketio.AsyncServer(async_mode='sanic')
 app = Sanic()
 sio.attach(app)
 
 
-async def background_task():
-    """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        await sio.sleep(10)
-        count += 1
-        await sio.emit('my response', {'data': 'Server generated event'},
-                       namespace='/test')
+class TestNamespace(socketio.AsyncNamespace):
 
+    def on_connect(self, sid, environ):
+        pass
 
-@app.listener('before_server_start')
-def before_server_start(sanic, loop):
-    sio.start_background_task(background_task)
+    def on_disconnect(self, sid):
+        pass
 
 
 @app.route('/')
 async def index(request):
-    with open('app.html') as f:
+    with open('index.html') as f:
         return html(f.read())
 
 
-@sio.on('my event', namespace='/test')
-async def test_message(sid, message):
-    await sio.emit('my response', {'data': message['data']}, room=sid,
-                   namespace='/test')
-
-
-@sio.on('my broadcast event', namespace='/test')
-async def test_broadcast_message(sid, message):
-    await sio.emit('my response', {'data': message['data']}, namespace='/test')
-
-
-@sio.on('join', namespace='/test')
-async def join(sid, message):
-    sio.enter_room(sid, message['room'], namespace='/test')
-    await sio.emit('my response', {'data': 'Entered room: ' + message['room']},
-                   room=sid, namespace='/test')
-
-
-@sio.on('leave', namespace='/test')
-async def leave(sid, message):
-    sio.leave_room(sid, message['room'], namespace='/test')
-    await sio.emit('my response', {'data': 'Left room: ' + message['room']},
-                   room=sid, namespace='/test')
-
-
-@sio.on('close room', namespace='/test')
-async def close(sid, message):
-    await sio.emit('my response',
-                   {'data': 'Room ' + message['room'] + ' is closing.'},
-                   room=message['room'], namespace='/test')
-    await sio.close_room(message['room'], namespace='/test')
-
-
-@sio.on('my room event', namespace='/test')
-async def send_room_message(sid, message):
-    await sio.emit('my response', {'data': message['data']},
-                   room=message['room'], namespace='/test')
-
-
-@sio.on('disconnect request', namespace='/test')
-async def disconnect_request(sid):
-    await sio.disconnect(sid, namespace='/test')
-
-
-@sio.on('connect', namespace='/test')
-async def test_connect(sid, environ):
-    await sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid,
-                   namespace='/test')
-
-
-@sio.on('disconnect', namespace='/test')
-def test_disconnect(sid):
-    print('Client disconnected')
-
-
-app.static('/static', './static')
+sio.register_namespace(TestNamespace('/test'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8001, debug=True)
+
