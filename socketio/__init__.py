96d4e5f257f0ec9bb68ce4b83f348e2899aa7f40
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-12-11 13:30:35 -0800

    Release 1.8.4

--- socketio/__init__.py
+++ socketio/__init__.py
@@ -19,7 +19,7 @@ else:  # pragma: no cover
     AsyncNamespace = None
     AsyncRedisManager = None
 
-__version__ = '1.8.3'
+__version__ = '1.8.4'
 
 __all__ = ['__version__', 'Middleware', 'Server', 'BaseManager',
            'PubSubManager', 'KombuManager', 'RedisManager', 'ZmqManager',
