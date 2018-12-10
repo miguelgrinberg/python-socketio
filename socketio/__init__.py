b2a58502e1e33ab9c735ee5a270733d53f33d2ad
Miguel Grinberg <miguel.grinberg@gmail.com>
2018-12-10 10:23:48 +0000

    Release 2.1.2

--- socketio/__init__.py
+++ socketio/__init__.py
@@ -21,7 +21,7 @@ else:  # pragma: no cover
     AsyncNamespace = None
     AsyncRedisManager = None
 
-__version__ = '2.1.1'
+__version__ = '2.1.2'
 
 __all__ = ['__version__', 'Server', 'BaseManager', 'PubSubManager',
            'KombuManager', 'RedisManager', 'ZmqManager', 'Namespace',
