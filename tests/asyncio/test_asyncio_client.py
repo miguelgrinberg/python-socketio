af2e2633ea7f6c3f205b9d9fadd06b08634cbae9
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-12-07 23:48:44 +0000

    More logging to track down intermittent test failures #nolog

--- tests/asyncio/test_asyncio_client.py
+++ tests/asyncio/test_asyncio_client.py
@@ -758,6 +758,8 @@ class TestAsyncClient(unittest.TestCase):
             side_effect=[ValueError, exceptions.ConnectionError, None]
         )
         _run(c._handle_reconnect())
+        print(wait_for.mock.call_count)  # logging to debug #572
+        print(wait_for.mock.call_args_list)
         assert wait_for.mock.call_count == 3
         assert [x[0][1] for x in asyncio.wait_for.mock.call_args_list] == [
             1.5,
@@ -779,6 +781,8 @@ class TestAsyncClient(unittest.TestCase):
             side_effect=[ValueError, exceptions.ConnectionError, None]
         )
         _run(c._handle_reconnect())
+        print(wait_for.mock.call_count)  # logging to debug #572
+        print(wait_for.mock.call_args_list)
         assert wait_for.mock.call_count == 3
         assert [x[0][1] for x in asyncio.wait_for.mock.call_args_list] == [
             1.5,
@@ -822,6 +826,8 @@ class TestAsyncClient(unittest.TestCase):
             side_effect=[ValueError, exceptions.ConnectionError, None]
         )
         _run(c._handle_reconnect())
+        print(wait_for.mock.call_count)  # logging to debug #572
+        print(wait_for.mock.call_args_list)
         assert wait_for.mock.call_count == 2
         assert [x[0][1] for x in asyncio.wait_for.mock.call_args_list] == [
             1.5,
