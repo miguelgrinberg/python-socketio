f0dddfc3e30cb41c4e41591edc84fcfce920a6e2
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-12-14 19:07:32 +0000

    Version 5.0.4dev

--- socketio/__init__.py
+++ socketio/__init__.py
@@ -27,7 +27,7 @@ else:  # pragma: no cover
     AsyncRedisManager = None
     AsyncAioPikaManager = None
 
-__version__ = '5.0.3'
+__version__ = '5.0.4dev'
 
 __all__ = ['__version__', 'Client', 'Server', 'BaseManager', 'PubSubManager',
            'KombuManager', 'RedisManager', 'ZmqManager', 'KafkaManager',
