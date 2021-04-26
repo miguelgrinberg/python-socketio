a37ab00b344e035adbdc532f2760c223db118c0f
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-04-26 19:41:09 +0100

    Remove Python 2 from PyPI classifiers

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
