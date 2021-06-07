98c7ac23f231b64cc8b8c51104b792d0cd5cf361
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-06-07 20:37:22 +0100

    Improved project structure

--- src/socketio/__init__.py
+++ src/socketio/__init__.py
@@ -27,9 +27,7 @@ else:  # pragma: no cover
     AsyncRedisManager = None
     AsyncAioPikaManager = None
 
-__version__ = '5.3.1dev'
-
-__all__ = ['__version__', 'Client', 'Server', 'BaseManager', 'PubSubManager',
+__all__ = ['Client', 'Server', 'BaseManager', 'PubSubManager',
            'KombuManager', 'RedisManager', 'ZmqManager', 'KafkaManager',
            'Namespace', 'ClientNamespace', 'WSGIApp', 'Middleware']
 if AsyncServer is not None:  # pragma: no cover
