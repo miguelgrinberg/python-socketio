dee9e82f753b9d1fd1307395928a40c13559cc17
Tolga Tarhan <ttarhan@users.noreply.github.com>
2021-10-25 20:22:05 -0700

    Fixes incorrect packet type for binary payloads with the msgpack serializer (Fixes #811)

--- src/socketio/msgpack_packet.py
+++ src/socketio/msgpack_packet.py
@@ -3,6 +3,14 @@ from . import packet
 
 
 class MsgPackPacket(packet.Packet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.packet_type == packet.BINARY_EVENT:
+            self.packet_type = packet.EVENT
+        elif self.packet_type == packet.BINARY_ACK:
+            self.packet_type = packet.ACK
+
     def encode(self):
         """Encode the packet for transmission."""
         return msgpack.dumps(self._to_dict())
