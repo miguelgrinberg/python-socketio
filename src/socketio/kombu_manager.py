5f83cd0f7b2911705eaf1a8cb9060afbee6eb456
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-09-02 22:59:14 +0100

    Renamed flask-socketio references to python-socketio (Fixes #1377)

--- src/socketio/kombu_manager.py
+++ src/socketio/kombu_manager.py
@@ -86,7 +86,7 @@ class KombuManager(PubSubManager):  # pragma: no cover
         return kombu.Exchange(self.channel, **options)
 
     def _queue(self):
-        queue_name = 'flask-socketio.' + str(uuid.uuid4())
+        queue_name = 'python-socketio.' + str(uuid.uuid4())
         options = {'durable': False, 'queue_arguments': {'x-expires': 300000}}
         options.update(self.queue_options)
         return kombu.Queue(queue_name, self._exchange(), **options)
