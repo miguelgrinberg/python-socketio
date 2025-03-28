d3efeb4efcbab36990870b2b60c34bfe32ad88e2
Miguel Grinberg <miguel.grinberg@gmail.com>
2025-03-28 20:01:57 +0000

    Add support for Redis Sentinel

--- src/socketio/async_redis_manager.py
+++ src/socketio/async_redis_manager.py
@@ -13,6 +13,7 @@ except ImportError:  # pragma: no cover
         RedisError = None
 
 from .async_pubsub_manager import AsyncPubSubManager
+from .redis_manager import parse_redis_sentinel_url
 
 
 class AsyncRedisManager(AsyncPubSubManager):  # pragma: no cover
@@ -29,15 +30,18 @@ class AsyncRedisManager(AsyncPubSubManager):  # pragma: no cover
             client_manager=socketio.AsyncRedisManager(url))
 
     :param url: The connection URL for the Redis server. For a default Redis
-                store running on the same host, use ``redis://``.  To use an
-                SSL connection, use ``rediss://``.
+                store running on the same host, use ``redis://``.  To use a
+                TLS connection, use ``rediss://``. To use Redis Sentinel, use
+                ``redis+sentinel://`` with a comma-separated list of hosts
+                and the service name after the db in the URL path. Example:
+                ``redis+sentinel://user:pw@host1:1234,host2:2345/0/myredis``.
     :param channel: The channel name on which the server sends and receives
                     notifications. Must be the same in all the servers.
     :param write_only: If set to ``True``, only initialize to emit events. The
                        default of ``False`` initializes the class for emitting
                        and receiving.
     :param redis_options: additional keyword arguments to be passed to
-                          ``aioredis.from_url()``.
+                          ``Redis.from_url()`` or ``Sentinel()``.
     """
     name = 'aioredis'
 
@@ -54,8 +58,16 @@ class AsyncRedisManager(AsyncPubSubManager):  # pragma: no cover
         super().__init__(channel=channel, write_only=write_only, logger=logger)
 
     def _redis_connect(self):
-        self.redis = aioredis.Redis.from_url(self.redis_url,
-                                             **self.redis_options)
+        if not self.redis_url.startswith('redis+sentinel://'):
+            self.redis = aioredis.Redis.from_url(self.redis_url,
+                                                 **self.redis_options)
+        else:
+            sentinels, service_name, connection_kwargs = \
+                parse_redis_sentinel_url(self.redis_url)
+            kwargs = self.redis_options
+            kwargs.update(connection_kwargs)
+            sentinel = aioredis.sentinel.Sentinel(sentinels, **kwargs)
+            self.redis = sentinel.master_for(service_name or self.channel)
         self.pubsub = self.redis.pubsub(ignore_subscribe_messages=True)
 
     async def _publish(self, data):
---
+++ tests/common/test_redis_manager.py
@@ -0,0 +1,24 @@
+import pytest
+
+from socketio.redis_manager import parse_redis_sentinel_url
+
+
+class TestPubSubManager:
+    def test_sentinel_url_parser(self):
+        with pytest.raises(ValueError):
+            parse_redis_sentinel_url('redis://localhost:6379/0')
+
+        assert parse_redis_sentinel_url(
+            'redis+sentinel://h1:6379,h2:6379'
+        ) == (
+            [('h1', 6379), ('h2', 6379)],
+            None,
+            {}
+        )
+        assert parse_redis_sentinel_url(
+            'redis+sentinel://user:password@h1:6379,h2:6379,h1:6380/0/myredis'
+        ) == (
+            [('h1', 6379), ('h2', 6379), ('h1', 6380)],
+            'myredis',
+            {'username': 'user', 'password': 'password', 'db': 0}
+        )
