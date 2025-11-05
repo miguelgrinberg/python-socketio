608a82fa2e022392aea1a05f270ec7f25378355f
phi <phi.friday@gmail.com>
2025-11-05 20:03:21 +0900

    fix: add warnings

--- src/socketio/msgpack_packet.py
+++ src/socketio/msgpack_packet.py
@@ -1,5 +1,8 @@
+import logging
 import msgpack
 from . import packet
+
+logger = logging.getLogger('socketio')
 
 
 class MsgPackPacket(packet.Packet):
@@ -8,14 +11,14 @@ class MsgPackPacket(packet.Packet):
     def encode(self):
         """Encode the packet for transmission."""
         return self._encode()
-    
+
     def _encode(self, **kwargs):
         return _msgpack.dumps(self._to_dict(), **kwargs)
 
     def decode(self, encoded_packet):
         """Decode a transmitted package."""
         return self._decode(encoded_packet)
-    
+
     def _decode(self, encoded_packet, **kwargs):
         decoded = msgpack.loads(encoded_packet, **kwargs)
         self.packet_type = decoded['type']
@@ -28,12 +31,26 @@ class MsgPackPacket(packet.Packet):
         dumps_default = kwargs.pop('dumps_default', None)
         ext_hook = kwargs.pop('ext_hook', msgpack.ExtType)
 
+        if args:
+            logger.warning(
+                'Some positional arguments to MsgPackPacket.configure() are '
+                'not used: %s',
+                args,
+            )
+        if kwargs:
+            logger.warning(
+                'Some keyword arguments to MsgPackPacket.configure() are '
+                'not used: %s',
+                kwargs,
+            )
+
         class ConfiguredMsgPackPacket(cls):
             def _encode(self, **kwargs):
                 kwargs.setdefault('default', dumps_default)
                 return super()._encode(**kwargs)
+
             def _decode(self, encoded_packet, **kwargs):
                 kwargs.setdefault('ext_hook', ext_hook)
                 return super()._decode(encoded_packet, **kwargs)
-        
+
         return ConfiguredMsgPackPacket
